496193d6b39a1517cbd753f2e436efd463adaa87
chaunnm <nnmchau781@gmail.com>
2025-12-16 22:53:57 +0700

    split file css, fix author variable

--- assets/scripts/donate.js
+++ assets/scripts/donate.js
@@ -3,7 +3,7 @@
     "Ho Ho Ho! Cảm ơn Santa Claus! 🎅",
     "Món quà này ấm hơn cả lò sưởi! 🔥",
     "Chúc bạn Giáng Sinh an lành 🎄",
-    "{{ site.author }} đã nhận được tín hiệu từ Bắc Cực ❄️",
+    "Châu đã nhận được tín hiệu từ Bắc Cực ❄️",
   ];
 
   let currentTimeout;
